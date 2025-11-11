a002586271c27fb6d6ffa59b6dc805d9a9399f4c
sherka <a.obidzhanov@kodeks.ru>
2025-11-11 17:47:48 +0300

    добавил отображением изменений с gitsings

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -58,5 +58,7 @@ require("lazy").setup({
 	{"hrsh7th/cmp-buffer"},
 	{"hrsh7th/cmp-path"},
 	{"hrsh7th/cmp-cmdline"},
-	{"hrsh7th/nvim-cmp"}
+	{"hrsh7th/nvim-cmp"},
+-- git
+	{"lewis6991/gitsigns.nvim"}
 })
