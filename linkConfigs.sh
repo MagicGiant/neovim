f07a6e687c6818ae4054d83280ab7abe9179eba8
sherkaLaptop <obidjanow@mail.ru>
2026-08-25 12:59:12 +0300

    .

--- linkConfigs.sh
+++ linkConfigs.sh
@@ -8,3 +8,4 @@ rm -rf ~/.config/waybar && ln -snf "$(pwd)/source/waybar" ~/.config/waybar
 rm -rf ~/.config/mouseless && ln -snf "$(pwd)/source/mouseless" ~/.config/mouseless
 rm -rf ~/.config/yazi && ln -snf "$(pwd)/source/yazi" ~/.config/yazi
 rm -rf ~/.config/opencode && ln -snf "$(pwd)/source/opencode" ~/.config/opencode
+rm ~/.config/herdr/config.toml && ln -snf "$(pwd)/source/herdr/config.toml"
